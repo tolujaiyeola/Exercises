c32b0adaa483a6bb1090474b2fa98d478b8c6c44
David Jaiyeola <tolu.jaiyeola@sasktel.com>
2018-10-12 10:35:33 -0600

    Console outputs using the fmt package.

---
+++ go/consoleOutput.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	str1 := "The quick red fox"
+	str2 := "jumped over"
+	str3 := "the lazy brown dog"
+	aNumber := 42
+	isTrue := true
+	stringLength, err := fmt.Println(str1, str2, str3) //returns integer and err
+
+	if err == nil {
+		fmt.Println(stringLength, err)
+	}
+
+	//Printf - this function lets you create strings that have place holders known as verbs that define how values with be formated
+	fmt.Printf("Value of aNumber: %v\n", aNumber)
+	fmt.Printf("Value of isTrue: %v\n", isTrue)
+	fmt.Printf("Value of aNumber as a float: %.2f\n", float64(aNumber))
+
+	fmt.Printf("Data types: %T, %T, %T,%T and %T\n", str1, str2, str3, aNumber, isTrue)
+
+	myString := fmt.Sprintf("Data types as variable: %T, %T, %T,%T and %T\n", str1, str2, str3, aNumber, isTrue)
+
+	fmt.Println(myString)
+
+}
